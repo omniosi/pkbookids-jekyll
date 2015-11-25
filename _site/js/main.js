74da69a922300ef56e7ab6c6c594a90e692b08aa
ornette coleman <omniosi@gmail.com>
2015-11-25 00:28:29 -0800

    setup

---
+++ _site/js/main.js
@@ -0,0 +1,40 @@
+(function(){
+
+var cart = document.getElementById('cart'),
+				moreless = document.getElementsByClassName('moreless')[0],
+				cont = document.getElementsByClassName('cont')[0];
+
+cont.style.display = 'none';
+moreless.addEventListener('click',openclose);
+
+window.addEventListener('resize',function(event){
+	var docwidth = window.innerWidth;
+
+	if(window.innerWidth>749){
+		cart.style.top = '0px';
+
+		cont.style.display = 'block';
+		moreless.style.display = 'none';
+
+	}	else {
+		cart.style.top = '25px';
+
+		cont.style.display = 'none';
+		moreless.style.display = 'block';
+
+	}
+	// console.log("addEventListener called");
+	// console.log("docwidth = "+docwidth);
+});
+
+function openclose(){
+	if(moreless.innerHTML === 'more'){
+		cont.style.display = 'block';
+		moreless.innerHTML = 'less';
+	}else{
+		cont.style.display = 'none';
+		moreless.innerHTML = 'more';
+	}
+}
+
+})();
